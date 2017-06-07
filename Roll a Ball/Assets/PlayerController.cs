817956fdd79b1049d8b0b0287c4554cb8f17e0b0
Jacob Byrd <Jacobbyrd48@gmail.com>
2017-06-07 11:22:22 -0400

    Added trigger effect to rotators completed 6 of 8

--- Roll a Ball/Assets/PlayerController.cs
+++ Roll a Ball/Assets/PlayerController.cs
@@ -2,24 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float speed;
 
     private Rigidbody rb;
 
-    void Start ()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
-    } 
+    }
 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce (movement * speed);
+        rb.AddForce(movement * speed);
     }
-} 
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick up"))
+        {
+            other.gameObject.SetActive (false);
+        }
+    }
+}
